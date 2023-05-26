297555134d408021da77cff58c2235ae9764d9ae
Antony Gilo <giloantony@gmail.com>
2023-05-26 14:46:45 +0300

    A bit of house cleaning on drilldown product page , making the sections responsive

--- admin/product-drilldown.php
+++ admin/product-drilldown.php
@@ -32,37 +32,43 @@ $row = mysqli_fetch_assoc($result);
 
       <div class="main-panel">
         <div class="content-wrapper">
-        <div class="row">
-                  <div class="col-md-6">
-                    <div class="card-body">
-                      <h4 class="card-title"><?php echo $row['product_name'] ?></h4>
-                      <p class="card-description">More Details About The Product</p>
-                      <div class="template-demo">
-                        <img src="<?php echo $row['product_pictures']; ?>" alt="picture">
-                      </div>
+            <div class="col-12 grid-margin">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="card-body">
+                                <h4 class="card-title"><?php echo $row['product_name'] ?></h4>
+                                <p class="card-description">More Details About The Product</p>
+                                <div class="template-demo">
+                                <img src="<?php echo $row['product_pictures']; ?>" alt="picture">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 pt-5" >
+                            <div class="card-body">
+                                <h4 class="card-title"> Product Name:</h4>
+                                <p class="card-description"> <?php echo $row['product_name'] ?> </p>
+                                <h4 class="card-title">Product Selling Price:</h4>
+                                <p class="card-description"> <?php echo $row['price'] ?> </p>
+                                <hr>
+                                <div class="template-demo">
+                                <p>
+                                <h4 class="card-title">Product Description:</h4>
+                                    <?php echo $row['product_description'] ?>
+                                </p>
+                                </div>
+                                <hr>
+                                <h4 class="card-title">Date Added To OPS System:</h4>
+                                <p class="card-description"> <?php echo $row['created_at'] ?> </p>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                  <div class="col-md-6 pt-5" >
-                    <div class="card-body">
-                      <h4 class="card-title"> Product Name:</h4>
-                      <p class="card-description"> <?php echo $row['product_name'] ?> </p>
-                      <h4 class="card-title">Product Selling Price:</h4>
-                      <p class="card-description"> <?php echo $row['price'] ?> </p>
-                      <hr>
-                      <div class="template-demo">
-                        <p>
-                        <h4 class="card-title">Product Description:</h4>
-                            <?php echo $row['product_description'] ?>
-                        </p>
-                      </div>
-                      <hr>
-                      <h4 class="card-title">Date Added To OPS System:</h4>
-                      <p class="card-description"> <?php echo $row['created_at'] ?> </p>
-                    </div>
-                  </div>
                 </div>
-          <!-- row end -->
+            </div>
         </div>
+      </div>
+
+        
         <!-- content-wrapper ends -->
         <!-- partial:./admin-partials/_footer.html -->
         <?php include('admin-partials/footer.php') ?>
